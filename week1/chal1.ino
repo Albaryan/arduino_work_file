d079beadc0a8a9e219b20007c36d61f3cd4c1c40
Albaryan <alper358tuna@gmail.com>
2023-02-04 16:42:10 +0300

    week1-chal1

---
+++ week1/chal1.ino
@@ -0,0 +1,15 @@
+void setup() {
+
+  Serial.begin(9600);         //Start communication with Serial Monitor
+  pinMode(9,INPUT_PULLUP);    //Set pin 9 as switch 
+  pinMode(8,OUTPUT);          //Set pin 8 as output
+}
+
+void loop() {
+ 
+  if (!digitalRead(9)) {              //Check if pin 9 is pressed
+    digitalWrite(8,!digitalRead(8));  //Reverse pin 8's value
+    while (!digitalRead(9));          //Loop if pin 9 is held
+  };
+  
+}
