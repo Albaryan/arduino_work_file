69e2a75368ba91c070f9beb842e838505ccffbe9
Albaryan <alper358tuna@gmail.com>
2023-02-23 14:01:05 +0300

    week3 chal3  arrangement

--- week3/chal3.ino
+++ week3/chal3.ino
@@ -1,23 +1,20 @@
 #include <Servo.h>
-
 Servo motor1;
 Servo motor2;
-int PWM1;
-int PWM2;
+int HORZ;
+int VERT;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   motor1.attach(3);
   motor2.attach(5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  PWM1=map(analogRead(A5),0,1023,0,180);
-  PWM2=map(analogRead(A4),0,1023,0,180);
+  HORZ=map(analogRead(A4),0,1023,0,180);
+  VERT=map(analogRead(A5),0,1023,0,180);
 
-  motor1.write(PWM1);
-  motor2.write(PWM2);
+  motor1.write(HORZ);
+  motor2.write(VERT);
 }
-
